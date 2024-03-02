e9c8487222dc04db325b839e2f083ab12d098c34
Sanjay Kumar <cusat.sanjay30@gmail.com>
2024-03-02 17:36:36 +0530

    some changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 
-console.log(localStorage.getItem("year"));
+// console.log(localStorage.getItem("year"));
 
 const makeTwoDigitNumber = (number) => {
   return number > 9 ? number : `0${number}`;
@@ -28,18 +28,26 @@ const toggleDateOfBirthSelector = () => {
   }
   isDOBOpen = !isDOBOpen;
 
-  console.log("Toggle", isDOBOpen);
+  // console.log("Toggle", isDOBOpen);
 };
 
 const updateAge = () => {
   const currentDate = new Date();
   const dateDiff = currentDate - dateOfBirth;
+
   const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365));
-  const month = Math.floor((dateDiff / (1000 * 60 * 60 * 24 * 365)) % 12);
-  const day = Math.floor(dateDiff / (1000 * 60 * 60 * 24)) % 30;
-  const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
-  const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
-  const second = Math.floor(dateDiff / 1000) % 60;
+  const remainingMilliseconds = dateDiff - year * 1000 * 60 * 60 * 24 * 365;
+  const month = Math.floor(remainingMilliseconds / (1000 * 60 * 60 * 24 * 30));
+  const day = Math.floor(
+    (remainingMilliseconds % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+  );
+  const hour = Math.floor(
+    (remainingMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minute = Math.floor(
+    (remainingMilliseconds % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  const second = Math.floor((remainingMilliseconds % (1000 * 60)) / 1000);
 
   yearEl.innerHTML = makeTwoDigitNumber(year);
   monthEl.innerHTML = makeTwoDigitNumber(month);
@@ -83,10 +91,9 @@ const setDOBHandler = () => {
   }
 
   contentToggler();
-  setInterval(() => updateAge(), 1000);
 };
 
-localStorageGetter();
+// localStorageGetter();
 contentToggler();
 
 settingCogEl.addEventListener("click", toggleDateOfBirthSelector);
